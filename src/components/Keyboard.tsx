97132d8b618823aff80876499957a7091f289882
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-16 17:30:12 +0200

    Fix keyboard colors

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -9,7 +9,7 @@ export default function Keyboard(props: {
       <div className={styles.Row}>
         {["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"].map((letter) => (
           <div
-            key="letter"
+            key={letter}
             className={styles.Key}
             data-state={props.letterStates[letter]}
           >
@@ -20,7 +20,7 @@ export default function Keyboard(props: {
       <div className={styles.Row}>
         {["a", "s", "d", "f", "g", "h", "j", "k", "l"].map((letter) => (
           <div
-            key="letter"
+            key={letter}
             className={styles.Key}
             data-state={props.letterStates[letter]}
           >
@@ -31,7 +31,7 @@ export default function Keyboard(props: {
       <div className={styles.Row}>
         {["z", "x", "c", "v", "b", "n", "m"].map((letter) => (
           <div
-            key="letter"
+            key={letter}
             className={styles.Key}
             data-state={props.letterStates[letter]}
           >
