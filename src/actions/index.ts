33798e548701a17408176f36c8a6f8a39f42cb6f
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-17 05:00:38 +0200

    New solution every day

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,11 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
+import solutions from './solutions.json'
+
+const solutionIndex = Math.floor((new Date() - new Date(2025, 7, 15)) / 86400000)
+const solution = solutions[solutionIndex]
+
 declare type LetterState =
     | "correct_position"
     | "correct_letter"
@@ -10,8 +15,6 @@ export const server = {
     evaluate: defineAction({
         input: z.string(),
         handler: (guess) => {
-
-            const solution = "cigar";
             const response: Array<LetterState> = [];
             for (let i = 0; i < solution.length; i++) {
                 if (solution[i] === guess[i]) {
