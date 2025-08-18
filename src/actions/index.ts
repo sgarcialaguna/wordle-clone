00aa53c17b63018f3db3be6c0b12c40134640c70
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-18 06:52:58 +0200

    Fix a bug in evaluation function

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -12,6 +12,16 @@ declare type LetterState =
     | "correct_letter"
     | "incorrect_letter";
 
+function getAllIndexesOfCharacter(needle: string, haystack: string) {
+    const indices = []
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle) {
+            indices.push(i)
+        }
+    }
+    return indices;
+}
+
 export const server = {
     evaluate: defineAction({
         input: z.string(),
@@ -24,7 +34,13 @@ export const server = {
                 if (solution[i] === guess[i]) {
                     response.push("correct_position");
                 } else if (solution.includes(guess[i])) {
-                    response.push("correct_letter");
+                    // Character may occur in string, but all other positions of string may have been guessed already
+                    const allIndexesOfCharacter = getAllIndexesOfCharacter(guess[i], solution)
+                    if (allIndexesOfCharacter.every(j => guess[i] === guess[j])) {
+                        response.push("incorrect_letter")
+                    } else {
+                        response.push("correct_letter");
+                    }
                 } else {
                     response.push("incorrect_letter");
                 }
